4d15409943335921d6ad1fc21c56550f1cd4999b
Nguyen Dong Thuc <nguyendongthuc13@gmail.com>
2021-07-22 22:46:15 +0700

    Test the new 2 autho factor

--- selenium/webdriver/Topic_07_Textbox_TextArea_Exercise.java
+++ selenium/webdriver/Topic_07_Textbox_TextArea_Exercise.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -70,7 +71,7 @@ public class Topic_07_Textbox_TextArea_Exercise {
 		*/
 		// Chrome Driver
 		System.setProperty("webdriver.chrome.driver", projectPath+"\\driverBrowsers\\chromedriver.exe");
-		driver = new ChromeDriver();
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		
